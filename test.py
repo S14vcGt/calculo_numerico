d02b13dbc7cd5582fd5fd07ab0ce9ad91e94ba7d
Sebastian Mata <sebasmata41@gmail.com>
2024-02-20 19:45:58 -0400

    nemotecnico

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ class IntegrationTest(ut.TestCase):#test de los metodos de integracion
 
         self.assertEqual(integral.riemman(f,0,1,4), 0.4385527739940128)
         self.assertEqual(integral.riemman(f,0,1,10000), 0.6094049985049783)
+
         self.assertEqual(integral.trapecio(f,0,1,4),0.6153294692906497)
         self.assertEqual(integral.trapecio(f,0,1,10000),0.6094757091830971)
 
@@ -19,6 +20,7 @@ class IntegrationTest(ut.TestCase):#test de los metodos de integracion
 
         self.assertEqual(integral.riemman(f,1,2,4), -1.6020877497586603)
         self.assertEqual(integral.riemman(f,1,2,10000), -2.3971330653828895)
+
         self.assertEqual(integral.trapecio(f,1,2,4), -2.2949338301069218)
         self.assertEqual(integral.trapecio(f,1,2,10000), -2.3974102038150362)
 
@@ -33,14 +35,18 @@ class NewtonTest(ut.TestCase):# test del metodo de Newton-Raphson
         dfx = lambda  x: -2*x*(mt.e**(-x**2))-2
         fx  = lambda  x: (mt.e**(-x**2))-2*x+1
    
-        X,Y,Z =  nr.newton_raphson(2,0.001,fx,dfx)
-        self.assertEqual(X,0.7744627493872464)
-        self.assertEqual(Y,1.8561324865995488e-07)
+        punto,error,tabla =  nr.newton_raphson(2,0.001,fx,dfx)
+        self.assertEqual(punto,0.7744627493872464)
+        self.assertEqual(error,1.8561324865995488e-07)
 
 class BiseccionTest(ut.TestCase):#test del metodo de biseccion
     def test1(self):
         f= lambda x: mt.e**x - 3*(x)**2
-        self.assertEqual(bc.biseccion(f,(0,1),0.04,6),(0.90625,0.034482758620689655,5))
+
+        punto, error, iteraciones = bc.biseccion(f,(0,1),0.04,6)
+        self.assertEqual(punto,0.90625)
+        self.asserEqual(error,0.034482758620689655)
+        self.assertEqual(iteraciones,5)
         
 if __name__=="__main__":
     ut.main()
